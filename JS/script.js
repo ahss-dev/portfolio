0aebfc0a72bc67e4e703fcb17362963e27844d0d
ahss-dev <antonio.hss.dev@gmail.com>
2026-02-10 21:10:04 -0300

    feat: adicionei animações na pagina css/js

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,63 @@
 const btnMobile = document.getElementById('btn-mobile');
+const nav = document.getElementById('nav');
+const menuLinks = document.querySelectorAll('#menu a');
 
 function toggleMenu(event) {
     if (event.type === 'touchstart') event.preventDefault();
-    const nav = document.getElementById('nav');
     nav.classList.toggle('active');
+
     const active = nav.classList.contains('active');
-    event.currentTarget.setAttribute('aria-expanded', nav.classList.contains('active'));
-    if (active) {
-        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-    } else {
-        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-    }
+    btnMobile.setAttribute('aria-expanded', active);
+
+    btnMobile.setAttribute(
+        'aria-label',
+        active ? 'Fechar Menu' : 'Abrir Menu'
+    );
 }
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('active');
+        btnMobile.setAttribute('aria-expanded', false);
+        btnMobile.setAttribute('aria-label', 'Abrir Menu');
+    });
+});
+
+//Menu aparecendo quando o usuário rola a página para cima e desaparecendo quando rola para baixo
+let lastScrollY = window.scrollY;
+const header = document.getElementById('header');
+
+window.addEventListener('scroll', () => {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY < lastScrollY) {
+        header.classList.remove('hide');
+    } 
+    else {
+        header.classList.add('hide');
+    }
+
+    lastScrollY = currentScrollY;
+});
+
+// Animação de fade-in para os elementos da página
+const animatedElements = document.querySelectorAll('.animate, .animate-up');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.3
+});
+
+animatedElements.forEach(element => {
+    observer.observe(element);
+});
+
